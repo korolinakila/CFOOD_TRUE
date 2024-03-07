d54b68fd14fc7da023de25e74f4a3c25c0e1e6e1
Sch1561-it-051 <matvey.makarov07@mail.ru>
2024-03-07 16:41:06 +0300

    URAAA!!!

--- app/src/main/java/com/example/cfood/MainActivity.java
+++ app/src/main/java/com/example/cfood/MainActivity.java
@@ -7,8 +7,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
-import android.widget.CompoundButton;
-import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
--- app/src/main/java/com/example/cfood/SecondMain.java
+++ app/src/main/java/com/example/cfood/SecondMain.java
@@ -12,10 +12,14 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 public class SecondMain extends AppCompatActivity {
     TextView textView;
     Button button;
     EditText inputText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,33 +27,57 @@ public class SecondMain extends AppCompatActivity {
         textView =  findViewById(R.id.textView);
         button =  findViewById(R.id.button);
         inputText = findViewById(R.id.inputText);
-        String api_url = "https://api.calorieninjas.com/v1/nutrition?query=";
-        String query = "pasta";
-        URL url = new URL(api_url + query);
-        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-        connection.setRequestMethod("GET");
-        connection.setRequestProperty("X-Api-Key", "H/9OuzUj5ZZcny14Lz8mpA==LVMG5rZBRAw2PTab");
+        Thread thread = new Thread(new Runnable() {
 
-        int status_code = connection.getResponseCode();
-        if (status_code == HttpURLConnection.HTTP_OK) {
-            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-            String line;
-            StringBuilder response = new StringBuilder();
-
-            while ((line = reader.readLine()) != null) {
-                response.append(line);
-            }
-            reader.close();
-
-            System.out.println(response.toString());}
-        button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                int soccer = Integer.parseInt(inputText.getText().toString());
+            public void run() {
+                try {
+                    String api_url = "https://api.calorieninjas.com/v1/nutrition?query=";
+                    String query = "pasta";
+                    URL url = null;
+                    try {
+                        url = new URL(api_url + query);
 
-                textView.setText("Количество калорий:"+ soccer);
+                        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                        connection.setRequestMethod("GET");
+                        connection.setRequestProperty("X-Api-Key", "H/9OuzUj5ZZcny14Lz8mpA==LVMG5rZBRAw2PTab");
 
+                        int status_code = connection.getResponseCode();
+                        if (status_code == HttpURLConnection.HTTP_OK) {
+                            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                            String line;
+                            StringBuilder response = new StringBuilder();
+
+                            while ((line = reader.readLine()) != null) {
+                                response.append(line);
+                            }
+                            reader.close();
+                            String jsonText = response.toString();
+                            JSONObject jsonObject = new JSONObject(jsonText);
+                            JSONArray items = jsonObject.getJSONArray("items");
+                            JSONObject item = (JSONObject) items.get(0);
+                            double calories = item.getDouble("calories");
+                            System.out.println(calories);}
+                        button.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                int soccer = Integer.parseInt(inputText.getText().toString());
+
+                                textView.setText("Количество калорий:"+ soccer);
+
+                            }
+                        });
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
             }
         });
+
+        thread.start();
+
+
     }
 }
